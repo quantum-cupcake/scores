36a7b7be9070b9f6698c06a280d03321078be607
vasya <zealot1318@mail.ru>
2022-05-11 15:13:54 +0400

    ScoresController

--- src/main/java/ru/pflb/scores/http/ScoresController.java
+++ src/main/java/ru/pflb/scores/http/ScoresController.java
@@ -1,17 +1,33 @@
 package ru.pflb.scores.http;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+import ru.pflb.scores.service.LoginService;
+import ru.pflb.scores.service.ScoreService;
 
 @RestController
 public class ScoresController {
 
+    private final ScoreService scoreServices;
+
+    @Autowired
+    public ScoresController(ScoreService scoreServices) {
+        this.scoreServices = scoreServices;
+    }
 
     @GetMapping(path = "/hiscores/{levelId}")
     //TODO implement
+    public String getHighScores(@PathVariable int levelId) {
+        return scoreServices.highScores(levelId);
+    }
 
-    @PostMapping(path = "/score/{levelId}/{userId}")
+    @PostMapping(path = "/score/{levelId}/{userId}",
+            consumes = MediaType.APPLICATION_JSON_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE)
     //TODO implement; pass sessionKey as Query Param (/.../../..?session={sessionKey})
+    public void record(@PathVariable int levelId, @PathVariable int userId, @RequestParam String sessionKey, @RequestBody int score) {
+
+        scoreServices.record(sessionKey,score,levelId);
+    }
 }
