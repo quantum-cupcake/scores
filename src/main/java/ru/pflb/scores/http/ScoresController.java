c5ce44a2dd8d53b0031a1426dc8293de54f38066
parrots38 <80542158+parrots38@users.noreply.github.com>
2022-05-21 03:41:55 +0300

    Update ScoresController.java
    Реализован метод getHighScores для запроса /hiscores/{levelId}. 
    Реализован метод recordScore для запроса /score/{levelId}/{userId}

--- src/main/java/ru/pflb/scores/http/ScoresController.java
+++ src/main/java/ru/pflb/scores/http/ScoresController.java
@@ -1,17 +1,29 @@
 package ru.pflb.scores.http;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import ru.pflb.scores.service.ScoreService;
+import ru.pflb.scores.service.impl.InMemoryScoreService;
 
 @RestController
 public class ScoresController {
 
+    private final ScoreService scoreService;
+
+    @Autowired
+    public ScoresController(ScoreService scoreService) {
+        this.scoreService = scoreService;
+    }
 
     @GetMapping(path = "/hiscores/{levelId}")
-    //TODO implement
+    public String getHighScores(@PathVariable int levelId){
+        return scoreService.highScores(levelId);
+    }
 
     @PostMapping(path = "/score/{levelId}/{userId}")
-    //TODO implement; pass sessionKey as Query Param (/.../../..?session={sessionKey})
+    public void recordScore(@PathVariable int levelId, @PathVariable int userId,
+                            @RequestParam String sessionKey, @RequestBody int score) {
+        // we have to append a header "Content-Type = application/json" in POST
+        scoreService.record(sessionKey, score, levelId);
+    }
 }
